89fb7308623348ea49645e82e2d2be5250d74060
camilaseguel <seguel.camilaalejandra@gmail.com>
2023-05-16 17:55:24 -0400

    cardvalidation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import validator from './validator.js';
+function verificavacio() {
+  //window.verificavacio = verificavacio;
+  //console.log(validator);
+  //TOMO EL NUMERO DE TARJETA DIGITADO POR EL USUARIO
+  //let creditCardNumber=document.getElementById("ntarjeta").value; //Variable
+  // constante
 
-console.log(validator);
+  //Alertas verificando vacios
+  // pido a mi codigo escuchar el "click" en mi botón principal
+  //para así revise mi función de verificar vacio (campos de texto sin llenar).
+
+  // console.log("hola mundo");
+  const nombre = document.getElementById("nombre").value; //determino 3 variables en la función y 1 constante
+  const numero = document.getElementById("ntarjeta").value;
+  const nCvv = document.getElementById("nCvv").value;
+  const masked = validator.maskify(numero);
+  //console.log(masked)
+
+  if (nombre === "") {//si, nombre es igual que "nada" da una alerta de "faltan datos" y así con las variantes siguientes
+    alert('faltan datos. Ingrese su nombre');
+  }
+  if (numero === "") {
+    alert('Faltan datos. Ingrese número de tarjeta');
+  }
+  if (nCvv === "") {
+    alert('Faltan datos. Ingrese su código cvv');
+  }
+  //Sale alerta en caso de número de tarjeta válido o inválido  
+
+  if (nombre !== "" && numero !== "" && nCvv !== "") { //Acá: si nombre no es igual a "nada" y si numero no es igual a "nada" y si nombre no es igual a "nada
+    const creditCardNumber = document.getElementById("ntarjeta").value;
+    const resultado = validator.isValid(creditCardNumber);
+    if (resultado === true) { //llamo a mi función isValid a verificar si el número es válido y si es así: salta alerta
+      alert("Tarjeta válida" + masked); //la alerta indica si la tarjeta es válida +  enmascarado de la tarjeta
+    }
+    else {         //si lo anterior no se cumple, salta alerta de tarjeta no válida con indicaciones al usuario
+      alert("Tarjeta no válida. Inténtalo de nuevo");
+    }
+  }
+}
+document.getElementById("botondecanje").addEventListener("click", (verificavacio));
+//console.log("validator");
+//enmascarar numeros digitados
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,37 @@
 const validator = {
-  // ...
-};
 
+  isValid: (creditCardNumber) => {
+    const convertAndReverse = creditCardNumber.toString().split("").reverse(); //convierto elnumero en cadena (.toString) luego, en un arreglo (.split) y luego lo invierto (.reverse)
+    for (let i = 0; i < convertAndReverse.length; i++) { //acá necesito que itere (For) por la longitud de cadena, 
+      if (i % 2 === 1) { //multiplicando por 2 la posición par y por 1 la impar, 
+        convertAndReverse[i] *= 2; //siempre desde el 0 hasta el último número de la cadena.
+      }
+      else {
+        convertAndReverse[i] *= 1;
+      }
+    }
+    for (let i = 0; i <= convertAndReverse.length; i++) { //Acá doy la indicación de que a los números mayores a nueve,
+      if (convertAndReverse[i] > 9) { //les reste 9, para que solo me quede uno número entero.
+        convertAndReverse[i] -= 9;
+      }
+    }
+    let total = 0; //solicito la suma de la toda la longitud de cadena
+    for (let i = 0; i < convertAndReverse.length; i++) { //retorno debe ser un total multiplo de 10
+      total += convertAndReverse[i];
+    }
+
+    // Log to console
+    return total % 10 === 0;
+
+  },
+
+  maskify: (creditCardNumber) => {
+    //console.log(creditCardNumber);
+    const visibles = 4; //de mi cifra de 12 números, requiero dejar visibles los últimos 4 números
+    const numeros = creditCardNumber.slice(0, -visibles).replace(/./g, "#"); //que esos 8 primeros se reemplacen por #
+    const ultimos = creditCardNumber.slice(-visibles); // que los últimos sean visibles
+    return (numeros + ultimos); //acá returnan unidos los primeros números enmascarados y los visibles (últimos 4)
+  }
+
+}
 export default validator;
